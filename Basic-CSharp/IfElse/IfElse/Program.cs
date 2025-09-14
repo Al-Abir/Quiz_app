d8e571f6e40bfc40dc3f567d0e78345d784e7940
Al-Abir <sheikhabir196@gmail.com>
2025-09-14 08:02:31 +0600

    first

---
+++ Basic-CSharp/IfElse/IfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿using IfElse;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program Started");
+
+        // Variables class method call
+        Variables.ShowVariables();
+
+        // Functions class method call
+       // Functions.SayHello();
+    }
+}
---
+++ Basic-CSharp/IfElse/IfElse/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class Variables
+    {
+        public static void ShowVariables()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"Sum: {a + b}");
+        }
+    }
+}
